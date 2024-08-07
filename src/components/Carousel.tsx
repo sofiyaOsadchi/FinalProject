d98ec3dc6443d9b76c4451b74bd9c1fc695fc3cf
sofiyaOsadchi <sofiyahason@gmail.com>
2024-08-08 01:38:28 +0300

    carousela

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
+/* import React from 'react';
 import { Carousel } from 'flowbite-react';
 
 const CarouselComponent: React.FC = () => {
     return (
         <div className="h-60 sm:h-70 xl:h-[25rem] 2xl:h-[40rem]">
             <Carousel pauseOnHover>
-              {/*   <img src="public/img/Elegant Dresses Facebook Cover (2400 x 1080 px) (1).png" alt="Elegant Dresses Cover" /> */}
                
                 <img src="\img\Brown & White Fashion Photo Collage New Collection Facebook Cover (2400 x 1000 px).png" alt="Elegant Dresses Cover" />
-                {/* <img src="public\img\Brown & White Fashion Photo Collage New Collection Facebook Cover.png"..." />
-                <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-                <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." /> */}
+                
+            </Carousel>
+        </div>
+    );
+}
+
+export { CarouselComponent }; */
+import React from 'react';
+import { Carousel } from 'flowbite-react';
+import './Carousel.scss';
+
+const CarouselComponent: React.FC = () => {
+    const isMobile = window.innerWidth < 768;
+
+    return (
+        <div className="h-60 sm:h-70 xl:h-[25rem] 2xl:h-[40rem] custom-carousel">
+            <Carousel pauseOnHover>
+                {isMobile ? (
+                    <img src="\img\Brown & White Fashion Photo Collage New Collection Facebook Cover (2400 x 1000 px) (2400 x 1200 px).png" alt="Elegant Dresses Cover Mobile" />
+                ) : (
+                    <img src="\img\Brown & White Fashion Photo Collage New Collection Facebook Cover (2400 x 1000 px).png" alt="Elegant Dresses Cover" />
+                )}
             </Carousel>
         </div>
     );
