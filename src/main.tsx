32bc43c05be46d47129ba7f5eb4becda0afc3ce0
sofiyaOsadchi <sofiyahason@gmail.com>
2024-09-03 17:42:33 +0300

    correcting GTM

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,7 @@ import { router } from "./routes/router.tsx";
 import { AuthContextProvider } from "./contexts/AuthContext.tsx";
 import { SearchProvider } from "./contexts/SearchContext.tsx";
 import { CartProvider } from "./contexts/CartContext.tsx";
-import TagManager from 'react-gtm-module';
 
-const tagManagerArgs = {
-  gtmId: 'GTM-K3ZGW6CX' 
-};
-
-TagManager.initialize(tagManagerArgs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <AuthContextProvider>
