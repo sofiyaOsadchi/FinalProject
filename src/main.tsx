dc1a41a65ec8bda7e6c4fd6b2053ae9cbee4d1e3
sofiyaOsadchi <sofiyahason@gmail.com>
2024-09-04 23:39:40 +0300

    context

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,17 @@ import { router } from "./routes/router.tsx";
 import { AuthContextProvider } from "./contexts/AuthContext.tsx";
 import { SearchProvider } from "./contexts/SearchContext.tsx";
 import { CartProvider } from "./contexts/CartContext.tsx";
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <AuthContextProvider>
     <SearchProvider>
       <CartProvider>
+        <DndProvider backend={HTML5Backend}>
     <RouterProvider router={router} />
+        </DndProvider>
       </CartProvider>
     </SearchProvider>
   </AuthContextProvider>
