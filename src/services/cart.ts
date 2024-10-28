0b32192fa5c586d7597efd82d805a63fb0c7a1c7
sofiyaOsadchi <sofiyahason@gmail.com>
2024-10-28 09:57:32 +0200

    carrt

--- src/services/cart.ts
+++ src/services/cart.ts
@@ -11,17 +11,6 @@ export const getCart = () => {
     });
 };
 
-/*************  ✨ Codeium Command ⭐  *************/
-/**
- * Adds a product to the cart. 
- * @param {string} productId - The id of the product.
- * @param {string} variantId - The id of the variant.
- * @param {number} quantity - The quantity to add to the cart.
- * @param {string} size - The size of the product.
- * @param {number} price - The price of the product.
- * @returns {Promise<AxiosResponse<any>>} - The response from the API.
- */
-/******  a4fc9471-b117-45f3-86af-068c8fcfa269  *******/
 export const addProductToCart = (productId: string, variantId: string, quantity: number, size: string, price: number) => {
     return axios.post(`${cartUrl}/add`, {
         productId,
